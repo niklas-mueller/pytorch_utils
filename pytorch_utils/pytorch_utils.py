3b4aa4a663f07255eeb465e7fac92e22a573bc18
Niklas Müller <niklas.mueller@gmx.net>
2022-12-02 17:11:06 +0100

    Added visualization and JPEG and color opponent channel transforms

--- pytorch_utils/pytorch_utils.py
+++ pytorch_utils/pytorch_utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import time
 from matplotlib.backends.backend_pdf import PdfPages # This is stupid but needs to be done in order to save as pdf pages
 from matplotlib import pyplot as plt
+from albumentations.augmentations.transforms import ImageCompression
 
 def visTensor(tensor, ch=0, allkernels=False, nrow=8, padding=1): 
     n,c,w,h = tensor.shape
@@ -49,6 +50,116 @@ def _get_device(model):
 def collate_fn(batch):
     batch = list(filter(lambda x: x is not None, batch))
     return torch.utils.data.dataloader.default_collate(batch)
+
+def rgb_to_opponent_space(img, normalize=False):
+    """rgb_to_opponent_space
+
+    Convert a image in RBG space to color opponent space, i.e., Intensity, Blue-Yellow (BY) opponent, Red-Green (RG) opponent.
+
+    Parameters
+    ----------
+    img: ndarray, list, PIL Image
+        Image to be converted
+    normalize: bool, optional
+        Whether to normalize pixel values by the maximum, by default False
+
+    Returns
+    ----------
+    ndarray
+        Array of length 3, with image converted to Intensity, BY, RG opponent, respectively.
+
+    Example
+    ----------
+    >>> 
+    """
+    o1 = 0.3 * img[:, :, 0] + 0.58 * img[:, :, 1] + \
+        0.11 * img[:, :, 2]   # Intensity/Luminance
+    o2 = 0.25 * img[:, :, 0] + 0.25 * img[:, :, 1] - \
+        0.5 * img[:, :, 2]   # BY opponent
+    o3 = 0.5 * img[:, :, 0] - 0.5 * \
+        img[:, :, 1]                        # RG opponent
+
+    if normalize:
+        ret = []
+        for _x in [o1, o2, o3]:
+            _max = _x.max()
+            ret.append(_x / _max)
+        return np.array(ret)
+
+    return np.array([o1, o2, o3])
+
+class ToJpeg(object):
+
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, sample):
+        jpeg = ImageCompression().apply(np.array(sample))
+
+        return jpeg
+
+class ToOpponentChannel(object):
+
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, sample):
+        ops = rgb_to_opponent_space(np.array(sample))
+
+        return ops.transpose((1,2,0))
+
+
+def get_result_figures(results:dict, model=None, result_manager=None, pdf_filename="result_figures.pdf", confusion_matrix=None):
+    figs = []
+
+    fig, ax = plt.subplots(1,1, figsize=(10,5))
+    ax.plot(results['training_losses'], label='Training')
+    ax.plot(results['validation_losses'], label='Validation')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Loss')
+    ax.legend()
+    ax.set_title('Losses during Training')
+    figs.append(fig)
+
+    ##############
+    # Accuracy bar plots on 
+    fig, ax = plt.subplots(1,1, figsize=(10,5))
+    train_acc = results['eval_trained_traindata']['accuracy']
+    test_acc = results['eval_trained_testdata']['accuracy']
+
+    ax.bar(x=[1,2], height=[train_acc,test_acc])
+    ax.set_xticks([1,2])
+    ax.set_xticklabels(['Training', 'Testing'])
+    ax.set_ylabel('Accuracy')
+    ax.set_xlabel('Dataset')
+    figs.append(fig)
+    ##############
+    fig, ax = plt.subplots(1,1, figsize=(10,5))
+    train_losses = list(results['eval_trained_traindata']['batch_losses'].values())
+    test_losses = list(results['eval_trained_testdata']['batch_losses'].values())
+
+    ax.boxplot(x=[train_losses,test_losses])
+    ax.set_xticks([1,2])
+    ax.set_xticklabels(['Training', 'Test'])
+    ax.set_ylabel('Average Batch Loss')
+    ax.set_xlabel('Dataset')
+    figs.append(fig)
+    ##############
+
+    if confusion_matrix is not None:
+        fig, ax = plt.subplots(1,1, figsize=(10,5))
+        bar = ax.imshow(confusion_matrix)
+        plt.colorbar(bar, ax=ax, label='Number of occurences', fraction=0.046)
+        ax.set_title(f"Confusion Matrix")
+        ax.set_xlabel('Predicted Class')
+        ax.set_ylabel('Original Class')
+        figs.append(fig)
+
+    ##############
+
+    if result_manager is not None:
+        result_manager.save_pdf(figs=figs, filename=pdf_filename)
+        
 
 def get_confusion_matrix(model, loader, n_classes, device=None):
     if device is None:
@@ -121,8 +232,8 @@ def train(model, trainloader, valloader, loss_fn, optimizer, device,
         for inputs, labels in trainloader:
 
             # Move data to device
-            inputs = inputs.to(device)
-            labels = labels.to(device)
+            inputs = inputs.to(device) #, dtype=torch.float32
+            labels = labels.to(device) #, dtype=torch.float32
 
             # Zero your gradients for every batch!
             optimizer.zero_grad()
